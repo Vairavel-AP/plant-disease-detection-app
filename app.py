1be41d7eaa936562d6cfb0691289700a0cad78a9
Vairavel-AP <vairavel.ap2006@gmail.com>
2025-09-16 09:33:28 +0530

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -148,4 +148,6 @@ def home():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render gives us PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
